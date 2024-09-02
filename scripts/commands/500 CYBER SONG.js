43a4b799a66162d2ef54e7511e50abfeb9c7fc28
༆...𝗠𝗛𝗔𝗕𝗨𝗕✞︎𝗥𝗔𝗛𝗠𝗔𝗡..༄ <mahabub678919@gmail.com>
2024-09-02 17:32:13 +0600

    Update and rename 500 CYBER SONG.txt to 500 CYBER SONG.js

--- scripts/commands/500 CYBER SONG.js
+++ scripts/commands/500 CYBER SONG.js
@@ -54,20 +54,17 @@ async function downloadMusicFromYoutube(link, path) {
 }
 
 
-
-module.exports = {
-  config: {
-    
-  name: "song1", 
-  version: "1.0.0", 
-  permission: 0,
-  credits: "Nayan",
-  description: "example",
-  prefix: true,
-  category: "Media", 
-  usages: "user", 
-  cooldowns: 5,
-},
+module.exports.config = {
+    name: "song",
+    version: "1.0.0",
+    permssion: 0,
+    credits: "D-Jukie",
+    description: "Phát nhạc thông qua link YouTube hoặc từ khoá tìm kiếm",
+    prefix: true,
+    category: "tiện ích",
+    usages: "[searchMusic]",
+    cooldowns: 0
+};
 
 handleReply: async function ({ api, event, handleReply }) {
     const axios = require('axios')
